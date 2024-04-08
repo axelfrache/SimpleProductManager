a43817a19b878544f334c7ca57d5b5d2a26ffa32
axelfrache <axelfrache@gmail.com>
2024-04-09 00:28:42 +0200

    Add home page

--- src/main/java/io/github/axelfrache/productmanager/controller/HomeController.java
+++ src/main/java/io/github/axelfrache/productmanager/controller/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String redirectToProducts() {
-        return "redirect:/products";
+    public String showHomePage() {
+        return "home";
     }
-
 }
